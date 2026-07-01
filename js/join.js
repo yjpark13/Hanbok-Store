7bb44316eee0f4be5bbba4dd111c8ca3db615b7e
psh4377 <psh4377@naver.com>
2026-07-01 12:23:22 +0900

    join.js 반 완성

---
+++ js/join.js
@@ -0,0 +1,118 @@
+const joinForm = document.getElementById("joinForm");
+
+const userName = document.getElementById("userName");
+const userId = document.getElementById("userId");
+const userPw = document.getElementById("userPw");
+const userPwCheck = document.getElementById("userPwCheck");
+
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const postcode = document.getElementById("postcode");
+const address = document.getElementById("address");
+const detailAddress = document.getElementById("detailAddress");
+
+const agreeCheck = document.getElementById("agreeCheck");
+
+joinForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const userNameValue = userName.value.trim();
+  const userIdValue = userId.value.trim();
+  const userPwValue = userPw.value.trim();
+  const userPwCheckValue = userPwCheck.value.trim();
+  const emailValue = email.value.trim();
+  const phoneValue = phone.value.trim();
+  const postcodeValue = postcode.value.trim();
+  const addressValue = address.value.trim();
+  const detailAddressValue = detailAddress.value.trim();
+
+  if (userNameValue === "") {
+    alert("이름을 입력해주세요.");
+    userName.focus();
+    return;
+  }
+
+  if (userIdValue === "") {
+    alert("아이디를 입력해주세요.");
+    userId.focus();
+    return;
+  }
+
+  if (userPwValue === "") {
+    alert("비밀번호를 입력해주세요.");
+    userPw.focus();
+    return;
+  }
+
+  if (userPwCheckValue === "") {
+    alert("비밀번호 확인을 입력해주세요.");
+    userPwCheck.focus();
+    return;
+  }
+
+  if (userPwValue !== userPwCheckValue) {
+    alert("비밀번호가 일치하지 않습니다.");
+    userPwCheck.focus();
+    return;
+  }
+
+  if (emailValue === "") {
+    alert("이메일을 입력해주세요.");
+    email.focus();
+    return;
+  }
+
+  if (phoneValue === "") {
+    alert("전화번호를 입력해주세요.");
+    phone.focus();
+    return;
+  }
+
+  if (postcodeValue === "") {
+    alert("우편번호를 입력해주세요.");
+    postcode.focus();
+    return;
+  }
+
+  if (addressValue === "") {
+    alert("주소를 입력해주세요.");
+    address.focus();
+    return;
+  }
+
+  if (detailAddressValue === "") {
+    alert("상세주소를 입력해주세요.");
+    detailAddress.focus();
+    return;
+  }
+
+  if (!agreeCheck.checked) {
+    alert("약관에 동의해주세요.");
+    agreeCheck.focus();
+    return;
+  }
+
+  if (!confirm("회원가입을 진행하시겠습니까?")) {
+    return;
+  }
+
+  //회원가입 진행. 회원가입 정보 객체.
+
+  const member = {
+    userName: userNameValue,
+    userId: userIdValue,
+    userPw: userPwValue,
+    email: emailValue,
+    phone: phoneValue,
+    postcode: postcodeValue,
+    address: addressValue,
+    detailAddress: detailAddressValue,
+    loginType: "normal",
+    provider: "일반 가입",
+
+    // 약관 동의 여부를 확인하는 부분, true가 될 수밖에 없으므로 필요는 없으나 유지보수를 위해 넣음.
+    termsAgreed: agreeCheck.checked,
+  };
+
+  console.log(member);
+});
